a58f2eca779457e4d2d9788a415db2a459039886
SuperPupperDoggo <65412993+SuperPupperDoggo@users.noreply.github.com>
2021-01-14 13:14:15 -0500

    Update sfx.h

--- src/sfx.h
+++ src/sfx.h
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
+/*
 #pragma once
 
 #include "studio.h"
@@ -51,3 +51,4 @@ struct Sfx
 
 void initSfx(Sfx*, tic_mem*, tic_sfx* src);
 void freeSfx(Sfx* sfx);
+*/
